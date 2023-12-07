5ae7273898be488c962d94aeb91fd1a4feeed3e3
Sergio Guzman <sergiocamachog12@gmail.com>
2023-12-07 09:54:29 -0400

    feature(person-router): implement functionality
    Add GET, POST, PUT & DELETE method

---
+++ src/services/person.service.js
@@ -0,0 +1,46 @@
+const Person = require('../database/models/person.model')
+
+const getAllPersons = async () => {
+  const person = await Person.findAll()
+  if (!person || person.length === 0) {
+    return { error: 'No person found' }
+  }
+
+  return person
+}
+const getPersonById = async (personId) => {
+  const person = await Person.findByPk(personId)
+  if (!person || person.length === 0) {
+    return { error: 'Person does not exist' }
+  }
+
+  return person
+}
+const createPerson = async (person) => {
+  return await Person.create(person)
+}
+const updatePerson = async (personId, updatedPerson) => {
+  const person = await Person.findByPk(personId)
+  if (!person || person.length === 0) {
+    return { error: 'Person does not exist' }
+  }
+
+  return await person.update(updatedPerson)
+}
+const deletePerson = async (personId) => {
+  const person = await Person.findByPk(personId)
+  if (!person || person.length === 0) {
+    return { error: 'Person does not exist' }
+  }
+
+  await person.destroy()
+  return { message: 'Person deleted successfully' }
+}
+
+module.exports = {
+  getAllPersons,
+  getPersonById,
+  createPerson,
+  updatePerson,
+  deletePerson
+}
