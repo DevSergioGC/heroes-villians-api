5783083f3850fa8fe68a8e5af112ba60fd4c47ab
Sergio Guzman <sergiocamachog12@gmail.com>
2023-12-07 15:14:02 -0400

    feature(user-router): add functionality
    Implement GET, POST & DELETE methods on user-router

--- src/utils/validation/schemas/user.schema.js
+++ src/utils/validation/schemas/user.schema.js
@@ -2,7 +2,8 @@ const Joi = require('joi')
 
 const userSchema = Joi.object({
   username: Joi.string().required(),
-  password: Joi.string().required().min(8).max(32)
+  password: Joi.string().required().min(8).max(32),
+  roleId: Joi.number().required()
 })
 
-module.exports = userSchema
+module.exports = { userSchema }
