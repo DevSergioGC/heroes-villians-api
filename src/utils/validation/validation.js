edd9d844e852a47f43ac37b58ebf28b2588ce194
DevSergioGC <sergiocamachog12@gmail.com>
2023-12-07 01:11:27 -0400

    feature(validation): validate every schema

---
+++ src/utils/validation/validation.js
@@ -0,0 +1,30 @@
+const { personSchema, updatePersonSchema } = require('./schemas/person.schema')
+const { heroSchema, updateHeroSchema } = require('./schemas/hero.schema')
+const {
+  villianSchema,
+  updateVillianSchema
+} = require('./schemas/villian.schema')
+const { roleSchema, updateRoleSchema } = require('./schemas/role.schema')
+const { userSchema } = require('./schemas/user.schema')
+
+const validatePerson = (data) => personSchema.validate(data)
+const validateUpdatePerson = (data) => updatePersonSchema.validate(data)
+const validateHero = (data) => heroSchema.validate(data)
+const validateUpdateHero = (data) => updateHeroSchema.validate(data)
+const validateVillian = (data) => villianSchema.validate(data)
+const validateUpdateVillian = (data) => updateVillianSchema.validate(data)
+const validateRole = (data) => roleSchema.validate(data)
+const validateUpdateRole = (data) => updateRoleSchema.validate(data)
+const validateUser = (data) => userSchema.validate(data)
+
+module.exports = {
+  validatePerson,
+  validateUpdatePerson,
+  validateHero,
+  validateUpdateHero,
+  validateVillian,
+  validateUpdateVillian,
+  validateRole,
+  validateUpdateRole,
+  validateUser
+}
