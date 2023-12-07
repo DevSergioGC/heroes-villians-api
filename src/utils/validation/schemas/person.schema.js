cfb5eea3a4da791624ff218ffb624ff5d767974b
DevSergioGC <sergiocamachog12@gmail.com>
2023-12-07 01:11:03 -0400

    feature(validation): implement schemas
    Define schemas to each model of the project

---
+++ src/utils/validation/schemas/person.schema.js
@@ -0,0 +1,16 @@
+import Joi from 'joi'
+
+const personSchema = Joi.object({
+  name: Joi.string().required(),
+  age: Joi.number().integer().min(18).max(100)
+})
+
+const updatePersonSchema = Joi.object({
+  name: Joi.string(),
+  age: Joi.number().integer().min(18).max(100)
+})
+
+module.exports = {
+  personSchema,
+  updatePersonSchema
+}
