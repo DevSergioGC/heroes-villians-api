05e62214b8b4c305b89f7e77d68df7ed60023fad
DevSergioGC <sergiocamachog12@gmail.com>
2023-12-08 08:49:36 -0400

    feature(models): add validation to db models

--- src/database/models/hero.model.js
+++ src/database/models/hero.model.js
@@ -2,22 +2,34 @@ const DataTypes = require('sequelize')
 const sequelize = require('../db.js')
 const Person = require('./person.model.js')
 
-const Hero = sequelize.define('hero', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const Hero = sequelize.define(
+  'hero',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    principalPower: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true
+    }
   },
-  principalPower: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    required: true
+  {
+    timestamps: false
   }
-})
+)
 
-Hero.belongsTo(Person, {
-  foreignKey: 'personId',
-  as: 'person'
-})
+Hero.belongsTo(
+  Person,
+  {
+    foreignKey: 'personId',
+    as: 'heroPerson'
+  },
+  {
+    timestamps: false
+  }
+)
 
 module.exports = Hero
