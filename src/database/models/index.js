e0baef9ae86239130865a9ec2905f23915e70693
Sergio Guzman <sergiocamachog12@gmail.com>
2023-12-11 11:11:52 -0400

    refactor(db-models): add index.js file
    Import & export all db models in one file to be more accesible

---
+++ src/database/models/index.js
@@ -0,0 +1,13 @@
+const Person = require('./person.model')
+const Hero = require('./hero.model')
+const Villian = require('./villian.model')
+const Location = require('./location.model')
+const User = require('./user.model')
+
+module.exports = {
+  Person,
+  Hero,
+  Villian,
+  Location,
+  User
+}
